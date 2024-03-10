9af080ed563d9bc6f5192fcc7c0260f7a50a6cd0
bohdancho <bogdan.chornocondratenko@gmail.com>
2024-03-11 00:52:48 +0100

    surround tweaks

--- lua/bohdancho/lazy/surround.lua
+++ lua/bohdancho/lazy/surround.lua
@@ -2,5 +2,14 @@ return {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    opts = { keymaps = { normal = "s" } },
+    opts = {
+        keymaps = { normal = "s" }, -- surround with `s` instead of `ys`
+        aliases = {
+            -- closing chars don't create whitespace, so alias opening chars to them
+            ["<"] = ">",
+            ["("] = ")",
+            ["{"] = "}",
+            ["["] = "]",
+        },
+    },
 }
