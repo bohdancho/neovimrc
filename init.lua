d022dbf5058c3f0204eb82c5d91198e236a891cd
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-03-27 18:32:15 +0100

    disable treesitter-context because it errors

--- init.lua
+++ init.lua
@@ -423,53 +423,50 @@ require("lazy").setup {
             },
             opts = {},
         },
-        --{
-        --    "nvim-treesitter/nvim-treesitter",
-        --    dependencies = {
-        --        "nvim-treesitter/nvim-treesitter-textobjects",
-        --        "nvim-treesitter/nvim-treesitter-context",
-        --        "dlvandenberg/tree-sitter-angular",
-        --    },
-        --    lazy = false,
-        --    build = ":TSUpdate",
-        --    config = function()
-        --        local configs = require "nvim-treesitter.configs"
-        --
-        --                require("treesitter-context").setup {
-        --                    multiline_threshold = 1, -- Maximum number of lines to show for a single context
-        --                }
-        --
-        --                ---@diagnostic disable-next-line: missing-fields
-        --                configs.setup {
-        --                    auto_install = true,
-        --                    highlight = {
-        --                        enable = true,
-        --                        disable = { "lua" },
-        --                    },
-        --                    indent = {
-        --                        enable = true,
-        --                    },
-        --                    textobjects = {
-        --                        select = {
-        --                            enable = true,
-        --                            lookahead = true,
-        --                            keymaps = {
-        --                                ["af"] = "@function.outer",
-        --                                ["if"] = "@function.inner",
-        --                                ["ac"] = "@class.outer",
-        --                                ["ic"] = "@class.inner",
-        --                            },
-        --                        },
-        --                    },
-        --                }
-        --
-        --                vim.filetype.add {
-        --                    pattern = {
-        --                        [".*%.component%.html"] = "htmlangular",
-        --                    },
-        --                }
-        --            end,
-        --        },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            dependencies = {
+                "nvim-treesitter/nvim-treesitter-textobjects",
+                -- "nvim-treesitter/nvim-treesitter-context",
+                "dlvandenberg/tree-sitter-angular",
+            },
+            lazy = false,
+            build = ":TSUpdate",
+            config = function()
+                -- require("treesitter-context").setup {
+                --     multiline_threshold = 1, -- Maximum number of lines to show for a single context
+                -- }
+
+                require("nvim-treesitter.configs").setup {
+                    auto_install = true,
+                    highlight = {
+                        enable = true,
+                        disable = { "lua" },
+                    },
+                    indent = {
+                        enable = true,
+                    },
+                    textobjects = {
+                        select = {
+                            enable = true,
+                            lookahead = true,
+                            keymaps = {
+                                ["af"] = "@function.outer",
+                                ["if"] = "@function.inner",
+                                ["ac"] = "@class.outer",
+                                ["ic"] = "@class.inner",
+                            },
+                        },
+                    },
+                }
+
+                vim.filetype.add {
+                    pattern = {
+                        [".*%.component%.html"] = "htmlangular",
+                    },
+                }
+            end,
+        },
 
         -- LSP
         {
