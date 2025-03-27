58e025bb98e4ab2a7bc2c7223209976ab3aeed5c
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-03-27 17:19:29 +0100

    add yazi, maybe fix wsl clipboard

--- lua/bohdancho/lazy/filetree.lua
+++ lua/bohdancho/lazy/filetree.lua
@@ -1,57 +1,58 @@
--- return {
--- "stevearc/oil.nvim",
--- dependencies = { "nvim-tree/nvim-web-devicons" },
--- lazy = false,
--- keys = {
---     {
---         "-",
---         function()
---             require("oil").open_float()
---         end,
---         desc = "Open file tree",
---     },
--- },
--- opts = {
---     keymaps = {
---         ["q"] = "actions.close",
---     },
---     view_options = {
---         show_hidden = true,
---     },
--- },
--- }
-
 ---@type LazySpec
 return {
-    "mikavilpas/yazi.nvim",
-    event = "VeryLazy",
-    dependencies = { "folke/snacks.nvim", lazy = true },
-    keys = {
-        {
-            "-",
-            mode = { "n", "v" },
-            "<cmd>Yazi<cr>",
-            desc = "Open yazi at the current file",
+    {
+        "stevearc/oil.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        lazy = false,
+        keys = {
+            {
+                "+",
+                function()
+                    require("oil").open_float()
+                end,
+                desc = "Open file tree",
+            },
         },
-        {
-            -- Open in the current working directory
-            "<leader>cw",
-            "<cmd>Yazi cwd<cr>",
-            desc = "Open the file manager in nvim's working directory",
-        },
-    },
-    ---@type YaziConfig | {}
-    opts = {
-        -- if you want to open yazi instead of netrw, see below for more info
-        open_for_directories = true,
-        keymaps = {
-            show_help = "<f1>",
+        opts = {
+            keymaps = {
+                ["q"] = "actions.close",
+            },
+            view_options = {
+                show_hidden = true,
+            },
         },
     },
-    -- 👇 if you use `open_for_directories=true`, this is recommended
-    init = function()
-        -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-        -- vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-    end,
+    {
+        "mikavilpas/yazi.nvim",
+        event = "VeryLazy",
+        dependencies = { "folke/snacks.nvim", lazy = true },
+        keys = {
+            {
+                "-",
+                mode = { "n", "v" },
+                "<cmd>Yazi<cr>",
+                desc = "Open yazi at the current file",
+            },
+            {
+                -- Open in the current working directory
+                "<leader>cw",
+                "<cmd>Yazi cwd<cr>",
+                desc = "Open the file manager in nvim's working directory",
+            },
+        },
+        ---@type YaziConfig | {}
+        opts = {
+            -- if you want to open yazi instead of netrw, see below for more info
+            open_for_directories = true,
+            keymaps = {
+                show_help = "<f1>",
+            },
+        },
+        -- 👇 if you use `open_for_directories=true`, this is recommended
+        init = function()
+            -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+            -- vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
+        end,
+    },
 }
--- lua/bohdancho/options.lua
+++ lua/bohdancho/options.lua
@@ -23,18 +23,20 @@ vim.keymap.set(
 
 local is_wsl = vim.api.nvim_eval 'has("wsl")' == 1
 if is_wsl then
-    vim.g.clipboard = {
-        name = "WslClipboard",
-        copy = {
-            ["+"] = "clip.exe",
-            ["*"] = "clip.exe",
-        },
-        paste = {
-            ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-            ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        },
-        cache_enabled = 0,
-    }
+    vim.cmd [[
+        let g:clipboard = {
+          \   'name': 'win32yank-wsl',
+          \   'copy': {
+          \      '+': 'win32yank.exe -i --crlf',
+          \      '*': 'win32yank.exe -i --crlf',
+          \    },
+          \   'paste': {
+          \      '+': 'win32yank.exe -o --lf',
+          \      '*': 'win32yank.exe -o --lf',
+          \   },
+          \   'cache_enabled': 0,
+          \ }
+    ]]
 end
 
 vim.opt.splitbelow = true
